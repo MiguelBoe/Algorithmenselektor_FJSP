ca8b55c09349e90adf3d4eb74eaa8bb58a1adca8
migue <miguel.boettcher@hotmail.com>
2022-12-11 18:11:44 +0100

    AlgorithmSelector

---
+++ algorithmSelector.py
@@ -0,0 +1,137 @@
+import pickle
+import pathlib
+import pandas as pd
+import numpy as np
+import math
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+# Konfigurationsbereich.
+#----------------------------------------------------------------------------------------------------------------------#
+data_path = '\\Users\\migue\\PycharmProjects\\Algorithmenselektor_JSP\\data'
+results_path = '\\Users\\migue\\PycharmProjects\\Algorithmenselektor_JSP\\results'
+models_path = '\\Users\\migue\\PycharmProjects\\Algorithmenselektor_JSP\\models'
+train_source = 'train'
+test_source = 'taillard'
+#----------------------------------------------------------------------------------------------------------------------#
+
+pathlib.Path(models_path).mkdir(parents=True, exist_ok=True)
+
+class AlgorithmSelector:
+    def __init__(self, mode, data_path, results_path, instance, model, train_source, test_source):
+        self.mode = mode
+        self.data_path = data_path
+        self.results_path = results_path
+        if mode == 'train':
+            self.train_source = train_source
+            self.test_source = test_source
+        elif mode == 'test':
+            self.instance = instance
+            self.model = model
+            self.source = test_source
+
+
+    def training(self):
+        with open(f'{self.data_path}\\{self.train_source}_data.pkl', 'rb') as in_file:
+            self.train_data = pickle.load(in_file)
+        with open(f'{self.data_path}\\{self.test_source}_data.pkl', 'rb') as in_file:
+            self.test_data = pickle.load(in_file)
+        self.train_set = self.get_instance_features(self, data = self.train_data, results=self.get_results(self, data = self.train_data, source = self.train_source))
+        self.test_set = self.get_instance_features(self, data = self.test_data, results=self.get_results(self, data = self.test_data, source = self.test_source))
+        self.define_attributes()
+        self.oversampling()
+        self.random_forest()
+        with open(f'{models_path}\\random_forest.pkl', 'wb') as out_file:
+            pickle.dump(self.random_forest_model, out_file)
+        print('\nTraining done!')
+
+
+    def get_selection(self):
+        self.X = self.get_instance_features(self, data = self.instance, results = None)
+        self.result = int(self.model.predict(self.X)[0])
+        return self.result
+
+
+    def get_instance_features(self, request, data, results):
+        if self.mode == 'test':
+            data = [data]
+        instance_features = pd.DataFrame()
+        for i in range(len(data)):
+            instance = data[i]
+            duration_intervals = self.get_duration_intervals(instance=instance)
+            features = {'num_machines': instance.num_machines, 'num_jobs': len(instance.list_of_jobs),
+                        'avg_job_duration': np.mean(instance.job_durations),
+                        'min_job_duration': min(instance.job_durations),
+                        'max_job_duration': max(instance.job_durations),
+                        'task_with_duration_[0:10]': duration_intervals[1],
+                        'task_with_duration_[11:20]': duration_intervals[2],
+                        'task_with_duration_[21:30]': duration_intervals[3],
+                        'task_with_duration_[31:40]': duration_intervals[4],
+                        'task_with_duration_[41:50]': duration_intervals[5],
+                        'task_with_duration_[51:60]': duration_intervals[6],
+                        'task_with_duration_[61:70]': duration_intervals[7],
+                        'task_with_duration_[71:80]': duration_intervals[8],
+                        'task_with_duration_[81:90]': duration_intervals[9],
+                        'task_with_duration_[91:100]': duration_intervals[10],
+                        'task_with_duration_[>100]': duration_intervals[11]}
+            if self.mode == 'train': features.update({'meta_better': results['meta_better'].iat[i]})
+            instance_features = instance_features.append(features, ignore_index=True)
+        if self.mode == 'train': instance_features['meta_better'] = instance_features['meta_better'].astype(int)
+        return instance_features
+
+
+    def get_results(self, request, data, source):
+        results_meta = pd.read_csv(f'{self.results_path}\\reports\\{source}_results_meta.csv', sep=',', index_col=0)
+        results_google = pd.read_csv(f'{self.results_path}\\reports\\{source}_results_google.csv', sep=',', index_col=0)
+        results_meta = results_meta.rename(columns={'Makespan': 'meta'})
+        results_google = results_google.rename(columns={'Makespan': 'google'})
+        results = pd.merge(results_meta, results_google, on='Instanz')
+        results['meta_better'] = np.NaN
+        for index, row in results.iterrows():
+            if row['meta'] < row['google']:
+                results['meta_better'].iat[index] = 1
+            else:
+                results['meta_better'].iat[index] = 0
+        return results
+
+
+    def get_duration_intervals(self, instance):
+        duration_intervals = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0}
+        for job in instance.list_of_jobs:
+            for task in job:
+                duration = task[1]
+                duration_intervals[min(math.ceil(duration/10),11)] += 1
+        duration_intervals = {k: round((duration_intervals[k]/(instance.num_machines*len(instance.list_of_jobs)))*100,3) for k in duration_intervals.keys()}
+        return duration_intervals
+
+
+    def define_attributes(self):
+        self.X_train = self.train_set[['num_machines', 'num_jobs', 'avg_job_duration', 'min_job_duration', 'max_job_duration',
+                               'task_with_duration_[0:10]', 'task_with_duration_[11:20]', 'task_with_duration_[21:30]', 'task_with_duration_[31:40]',
+                               'task_with_duration_[41:50]', 'task_with_duration_[51:60]', 'task_with_duration_[61:70]', 'task_with_duration_[71:80]',
+                               'task_with_duration_[81:90]', 'task_with_duration_[91:100]', 'task_with_duration_[>100]']]
+        self.y_train = self.train_set['meta_better']
+        self.X_test = self.test_set[['num_machines', 'num_jobs', 'avg_job_duration', 'min_job_duration', 'max_job_duration',
+                               'task_with_duration_[0:10]', 'task_with_duration_[11:20]', 'task_with_duration_[21:30]', 'task_with_duration_[31:40]',
+                               'task_with_duration_[41:50]', 'task_with_duration_[51:60]', 'task_with_duration_[61:70]', 'task_with_duration_[71:80]',
+                               'task_with_duration_[81:90]', 'task_with_duration_[91:100]', 'task_with_duration_[>100]']]
+        self.y_test = self.test_set['meta_better']
+
+
+    def oversampling(self):
+        rus = RandomOverSampler(random_state=0)
+        self.X_train, self.y_train = rus.fit_resample(self.X_train, self.y_train)
+
+
+    def random_forest(self):
+        self.random_forest_model = RandomForestClassifier(max_depth=10).fit(self.X_train, self.y_train)
+        self.results = pd.DataFrame({'prediction':self.random_forest_model.predict(self.X_test)}, index=self.X_test.index)
+        self.results['y_test'] = self.y_test
+        self.score = accuracy_score(self.results['y_test'], self.results['prediction'])
+
+if __name__ == "__main__":
+    AlgorithmSelector(mode = 'train', data_path = data_path, results_path = results_path, instance = None, model = None, train_source = train_source, test_source = test_source).training()
--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import plotly.figure_factory as ff
 from giffler_thompson import get_predecessor
 from tabuSearch import TabuSearch
 from jobList import JobList
+from algorithmSelector import AlgorithmSelector
 import pickle
 import time
 import pathlib
@@ -29,16 +30,17 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Konfigurationsbereich
 #----------------------------------------------------------------------------------------------------------------------#
-# Definition des Dateipfades.
+# Definition der Dateipfade.
 data_path = '\\Users\\migue\\PycharmProjects\\Algorithmenselektor_JSP\\data'
 results_path = '\\Users\\migue\\PycharmProjects\\Algorithmenselektor_JSP\\results'
+models_path = '\\Users\\migue\\PycharmProjects\\Algorithmenselektor_JSP\\models'
 
 # Auswahl der Instanz des generierten Datensatzes.
-source = 'random' # random, train, taillard
+source = 'taillard' # random, train, taillard
 instance = None
 
 # Auswahl des Solvers und Definition des Zeitlimits der Planung.
-solver = 'meta'  # google, meta, algorithm_selector
+solver = 'algorithm_selector'  # google, meta, algorithm_selector
 time_limit_in_seconds = 5
 
 # Konfiguration der Metaheuristik.
@@ -52,10 +54,15 @@ visualization_mode = False
 # Erstellung des Ordners, in welchem die Ergebnisse abgespeichert sind.
 pathlib.Path(f'{results_path}\\reports').mkdir(parents=True, exist_ok=True)
 pathlib.Path(f'{results_path}\\schedules').mkdir(parents=True, exist_ok=True)
+pathlib.Path(models_path).mkdir(parents=True, exist_ok=True)
 
 # Einlesen der Daten.
 with open(f'{data_path}\\{source}_data.pkl', 'rb') as in_file:
     data = pickle.load(in_file)
+
+if solver == 'algorithm_selector':
+    with open(f'{models_path}\\random_forest.pkl', 'rb') as in_file:
+        model = pickle.load(in_file)
 
 results = pd.DataFrame()
 schedule = {}
@@ -63,7 +70,7 @@ for instance in range(len(data)):
     # Auswahl des Solvers mit entsprechendem Konfigurationsparameter.
     if solver == "google":
         # Ausführung Google CP_Solver.
-        assigned_jobs, all_machines, google_makespan = ortools_scheduler(data=data[instance].list_of_jobs, time_limit_in_seconds=time_limit_in_seconds)
+        assigned_jobs, all_machines, makespan = ortools_scheduler(data=data[instance].list_of_jobs, time_limit_in_seconds=time_limit_in_seconds)
         # Transformation der Daten in das richtige Format für die Visualisierung der Planung mit Hilfe von Plotly in einem Gantt-Diagramm.
         schedule_list = visualize_schedule(assigned_jobs=assigned_jobs, all_machines=all_machines, plan_date=0)
     elif solver == "meta":
@@ -75,11 +82,20 @@ for instance in range(len(data)):
         best_solution = TabuSearch(current_solution=init_solution, max_iter=max_iter, tabu_list_length=int((len(data[instance].list_of_jobs)+data[instance].num_machines)/2), time_limit_in_seconds=time_limit_in_seconds).smart_solve(timeout)
         # Transformation der Daten in das richtige Format für die Visualisierung der Planung mit Hilfe von Plotly in einem Gantt-Diagramm.
         schedule_list = get_schedule_list(best_solution.schedule)
-        print(f'Best solution with TabuSearch found with a makespan of {best_solution.makespan}')
+        makespan = best_solution.makespan
+        print(f'Best solution with TabuSearch found with a makespan of {makespan}')
     elif solver == 'algorithm_selector':
         timeout = time.time() + time_limit_in_seconds
-
-        print()
+        selection = AlgorithmSelector(mode = 'test', data_path = data_path, results_path = results_path, instance=data[instance], model = model, train_source = None, test_source = source).get_selection()
+        if selection == 0:
+            assigned_jobs, all_machines, makespan = ortools_scheduler(data=data[instance].list_of_jobs, time_limit_in_seconds=time_limit_in_seconds)
+            schedule_list = visualize_schedule(assigned_jobs=assigned_jobs, all_machines=all_machines, plan_date=0)
+        elif selection == 1:
+            init_solution = giffler_thompson(data[instance], priority_rule)
+            best_solution = TabuSearch(current_solution=init_solution, max_iter=max_iter, tabu_list_length=int((len(data[instance].list_of_jobs) + data[instance].num_machines) / 2), time_limit_in_seconds=time_limit_in_seconds).smart_solve(timeout)
+            schedule_list = get_schedule_list(best_solution.schedule)
+            makespan = best_solution.makespan
+            print(f'Best solution with TabuSearch found with a makespan of {makespan}')
 
     # Visualisierng der Planung mit Hilfe von Plotly in einem Gantt-Diagramm.
     if visualization_mode:
@@ -87,9 +103,9 @@ for instance in range(len(data)):
         fig.layout.xaxis.type = "linear"
         fig.show()
 
-    if solver == 'meta': results = results.append({'Instanz':instance, 'Makespan': best_solution.makespan}, ignore_index=True)
-    elif solver == 'google': results = results.append({'Instanz': instance, 'Makespan': google_makespan}, ignore_index=True)
-    schedule.update({f'Instanz_{instance}': best_solution})
+    if solver == 'algorithm_selector': results = results.append({'Instanz':instance, 'Makespan': makespan, 'Solver': selection}, ignore_index=True)
+    else: results = results.append({'Instanz':instance, 'Makespan': makespan}, ignore_index=True)
+    schedule.update({f'Instanz_{instance}': schedule_list})
     print(f'Instance {instance} done!')
 
 results.to_csv(f'{results_path}\\reports\\{source}_report_{solver}.csv', sep=',')
